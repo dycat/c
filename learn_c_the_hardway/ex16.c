d757e3562a208bbc175e53f57431e1c7b0f1fb39
dycat <8856528+dycat@users.noreply.github.com>
2022-07-20 09:13:40 +0800

    add ex16

---
+++ learn_c_the_hardway/ex16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+    char *name;
+    int age;
+    int height;
+    int weight;
+};
+
+struct Person *Person_create(char *name, int age, int height, int weight) {
+    struct Person *who = malloc(sizeof(struct Person));
+    assert(who != NULL);
+
+    who->name = strdup(name);
+    who->age = age;
+    
+}
