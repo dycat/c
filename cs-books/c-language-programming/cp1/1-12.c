15f0f5dfa6e1c7a037243dc14bd4a567ea143188
dycat <8856528+dycat@users.noreply.github.com>
2023-09-07 20:23:28 +0800

    create 1-11.c 1-12.c

---
+++ cs-books/c-language-programming/cp1/1-12.c
@@ -0,0 +1,16 @@
+/*
+    1-12 Write a program that prints its input one word per line.
+*/
+#include <stdio.h>
+
+int main() {
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            putchar('\n');
+        } else {
+            putchar(c);
+        }
+    }
+    
+}
