b2a5a2d058631d5e49394b12721f512a04b6610b
dycat <8856528+dycat@users.noreply.github.com>
2023-09-10 16:00:45 +0800

    Create ex2-10.c

---
+++ cs-books/c-language-programming/cp2/ex2-10.c
@@ -0,0 +1,14 @@
+/*
+    ex 2-10 Rewrite the function lower, which converts upper case letters to lower case, with a conditional expression instead of if-else.
+*/
+#include <stdio.h>
+
+int lower(int c);
+
+int main() {
+    printf("%c\n", lower('U'));
+}
+
+int lower(int c) {
+    return (c >= 'A' && c<= 'Z') ? (c + 'a' - 'A') : c;
+}
