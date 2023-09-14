58cd838844a63c630198ae389c904a6261497d6d
dycat <8856528+dycat@users.noreply.github.com>
2023-09-14 21:07:59 +0800

    Update ex5-11.c

--- cs-books/c-language-programming/cp5/ex5-11.c
+++ cs-books/c-language-programming/cp5/ex5-11.c
@@ -6,43 +6,48 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define MAXNUM 100
+
+void push(int x);
+int pop();
+
+int stack[MAXNUM];
+int top;
 
 int main(int argc, char *argv[]) {
     
     int n = 0;
-    int stack[100];
+    int op1, op2;
 
-    for (int i = 0; i < argc; i ++) {
-        if (isdigit(argv[i])) {
-            push(argv[i], stack);
+    for (int i = 1; i < argc; i ++) {
+        if (isdigit(argv[i][0])) {
+            push(argv[i][0]);
         } else {
-            switch (argv[i])
-            {
+            switch (argv[i][0]) {
             case '+':
-                int op1 = pop(stack);
-                int op2 = pop(stack);
+                op1 = pop();
+                op2 = pop();
                 int sum = op1 + op2;
-                push(sum, stack);
+                push(sum);
                 break;
             case '-':
-                int op1 = pop(stack);
-                int op2 = pop(stack);
+                op1 = pop();
+                op2 = pop();
                 int minus = op1 - op2;
-                push(minus, stack);
+                push(minus);
                 break;
             case '*':
-                int op1 = pop(stack);
-                int op2 = pop(stack);
-                int multiply = op1 - op2;
-                push(multiply, stack);
+                op1 = pop();
+                op2 = pop();
+                int multiply = op1 * op2;
+                push(multiply);
                 break;
             case '/':
-                int op1 = pop(stack);
-                int op2 = pop(stack);
-                int divide = op1 - op2;
-                push(divide, stack);
+                op1 = pop();
+                op2 = pop();
+                int divide = op1 / op2;
+                push(divide);
                 break;
-            
             default:
                 break;
             }
@@ -52,10 +57,20 @@ int main(int argc, char *argv[]) {
     printf("%d", stack[0]);
 }
 
-void push(int x, int stack[]) {
+void push(int x) {
+    if (top >= MAXNUM) {
+        return ;
+    }
+    top += 1;
+    stack[top] = x;
 
 }
 
-int *pop(int stack[]) {
-
+int pop() {
+    if (top <= 0) {
+        return -1;
+    }
+    int return_value = stack[top];
+    top -= 1;
+    return return_value; 
 }
