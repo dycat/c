a121df206fbec86f778f9e97e1c3d69541f66e50
dycat <8856528+dycat@users.noreply.github.com>
2023-09-21 20:57:19 +0800

    chore: add files

--- cs-books/CSAPP/mstore.s
+++ cs-books/CSAPP/mstore.s
@@ -1,25 +1,72 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 13, 0	sdk_version 14, 0
-	.globl	_multstore                      ; -- Begin function multstore
-	.p2align	2
-_multstore:                             ; @multstore
-	.cfi_startproc
-; %bb.0:
-	stp	x20, x19, [sp, #-32]!           ; 16-byte Folded Spill
-	.cfi_def_cfa_offset 32
-	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
-	add	x29, sp, #16
-	.cfi_def_cfa w29, 16
-	.cfi_offset w30, -8
-	.cfi_offset w29, -16
-	.cfi_offset w19, -24
-	.cfi_offset w20, -32
+	.arch armv8-a
+	.text
+	.align	2
+	.globl _multstore
+_multstore:
+LFB0:
+	stp	x29, x30, [sp, -32]!
+LCFI0:
+	mov	x29, sp
+	str	x19, [sp, 16]
+LCFI1:
 	mov	x19, x2
 	bl	_mult2
 	str	x0, [x19]
-	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
-	ldp	x20, x19, [sp], #32             ; 16-byte Folded Reload
+	ldr	x19, [sp, 16]
+	ldp	x29, x30, [sp], 32
+LCFI2:
 	ret
-	.cfi_endproc
-                                        ; -- End function
-.subsections_via_symbols
+LFE0:
+	.section __TEXT,__eh_frame,coalesced,no_toc+strip_static_syms+live_support
+EH_frame1:
+	.set L$set$0,LECIE1-LSCIE1
+	.long L$set$0
+LSCIE1:
+	.long	0
+	.byte	0x1
+	.ascii "zR\0"
+	.uleb128 0x1
+	.sleb128 -8
+	.byte	0x1e
+	.uleb128 0x1
+	.byte	0x10
+	.byte	0xc
+	.uleb128 0x1f
+	.uleb128 0
+	.align	3
+LECIE1:
+LSFDE1:
+	.set L$set$1,LEFDE1-LASFDE1
+	.long L$set$1
+LASFDE1:
+	.long	LASFDE1-EH_frame1
+	.quad	LFB0-.
+	.set L$set$2,LFE0-LFB0
+	.quad L$set$2
+	.uleb128 0
+	.byte	0x4
+	.set L$set$3,LCFI0-LFB0
+	.long L$set$3
+	.byte	0xe
+	.uleb128 0x20
+	.byte	0x9d
+	.uleb128 0x4
+	.byte	0x9e
+	.uleb128 0x3
+	.byte	0x4
+	.set L$set$4,LCFI1-LCFI0
+	.long L$set$4
+	.byte	0x93
+	.uleb128 0x2
+	.byte	0x4
+	.set L$set$5,LCFI2-LCFI1
+	.long L$set$5
+	.byte	0xde
+	.byte	0xdd
+	.byte	0xd3
+	.byte	0xe
+	.uleb128 0
+	.align	3
+LEFDE1:
+	.ident	"GCC: (Homebrew GCC 13.2.0) 13.2.0"
+	.subsections_via_symbols
