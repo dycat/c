232577f3502098dcdaac9a5e11ae1e18e36d06cd
dycat <8856528+dycat@users.noreply.github.com>
2023-09-24 10:20:11 +0800

    compile mstore.c in gcc

--- cs-books/CSAPP/mstore.s
+++ cs-books/CSAPP/mstore.s
@@ -1,72 +1,25 @@
-	.arch armv8-a
-	.text
-	.align	2
-	.globl _multstore
-_multstore:
-LFB0:
-	stp	x29, x30, [sp, -32]!
-LCFI0:
-	mov	x29, sp
-	str	x19, [sp, 16]
-LCFI1:
-	mov	x19, x2
-	bl	_mult2
-	str	x0, [x19]
-	ldr	x19, [sp, 16]
-	ldp	x29, x30, [sp], 32
-LCFI2:
-	ret
-LFE0:
-	.section __TEXT,__eh_frame,coalesced,no_toc+strip_static_syms+live_support
-EH_frame1:
-	.set L$set$0,LECIE1-LSCIE1
-	.long L$set$0
-LSCIE1:
-	.long	0
-	.byte	0x1
-	.ascii "zR\0"
-	.uleb128 0x1
-	.sleb128 -8
-	.byte	0x1e
-	.uleb128 0x1
-	.byte	0x10
-	.byte	0xc
-	.uleb128 0x1f
-	.uleb128 0
-	.align	3
-LECIE1:
-LSFDE1:
-	.set L$set$1,LEFDE1-LASFDE1
-	.long L$set$1
-LASFDE1:
-	.long	LASFDE1-EH_frame1
-	.quad	LFB0-.
-	.set L$set$2,LFE0-LFB0
-	.quad L$set$2
-	.uleb128 0
-	.byte	0x4
-	.set L$set$3,LCFI0-LFB0
-	.long L$set$3
-	.byte	0xe
-	.uleb128 0x20
-	.byte	0x9d
-	.uleb128 0x4
-	.byte	0x9e
-	.uleb128 0x3
-	.byte	0x4
-	.set L$set$4,LCFI1-LCFI0
-	.long L$set$4
-	.byte	0x93
-	.uleb128 0x2
-	.byte	0x4
-	.set L$set$5,LCFI2-LCFI1
-	.long L$set$5
-	.byte	0xde
-	.byte	0xdd
-	.byte	0xd3
-	.byte	0xe
-	.uleb128 0
-	.align	3
-LEFDE1:
-	.ident	"GCC: (Homebrew GCC 13.2.0) 13.2.0"
-	.subsections_via_symbols
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 12, 0	sdk_version 13, 1
+	.globl	_multstore                      ## -- Begin function multstore
+	.p2align	4, 0x90
+_multstore:                             ## @multstore
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	pushq	%rbx
+	pushq	%rax
+	.cfi_offset %rbx, -24
+	movq	%rdx, %rbx
+	callq	_mult2
+	movq	%rax, (%rbx)
+	addq	$8, %rsp
+	popq	%rbx
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+.subsections_via_symbols
