b2475f3e14eba955b31d67c43e0af8b6915d3573
dycat <8856528+dycat@users.noreply.github.com>
2023-09-25 20:33:23 +0800

    add more function example

---
+++ cs-books/CSAPP/arith.s
@@ -0,0 +1,17 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 12, 0	sdk_version 13, 1
+	.globl	_arith                          ## -- Begin function arith
+	.p2align	4, 0x90
+_arith:                                 ## @arith
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+.subsections_via_symbols
