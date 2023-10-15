b20c1381a4ebdbe8e7b62cf9f935bffc08d0c02d
dycat <8856528+dycat@users.noreply.github.com>
2023-10-15 10:46:25 +0800

    Update udpclient.py

--- cs-books/computer_networking_a_top_down_apporach/udpclient.py
+++ cs-books/computer_networking_a_top_down_apporach/udpclient.py
@@ -2,6 +2,6 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message = input('Input sentences: ')
-client.connect(('localhost', 12000))
-client.send(message.encode())
+
+client.sendto(message.encode(), ('localhost', 12000))
 client.close()
