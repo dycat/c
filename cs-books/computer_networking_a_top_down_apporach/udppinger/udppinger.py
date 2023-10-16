fd84296e25a2dbc1f94dc88ceac1b6d48ebbdd8a
dycat <8856528+dycat@users.noreply.github.com>
2023-10-16 22:02:12 +0800

    Update udppinger.py

--- cs-books/computer_networking_a_top_down_apporach/udppinger/udppinger.py
+++ cs-books/computer_networking_a_top_down_apporach/udppinger/udppinger.py
@@ -1,21 +1,27 @@
 import socket
 import time
 
-pinger = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-pinger.settimeout(1)
+def ping():
 
-for i in range(1, 11): 
-        try:
-            startTime = time.time()
-            pinger.sendto(f"Ping {i} {startTime}".encode(), ('localhost', 12000))
-            # print(f"Ping {i} {startTime}")
-            message, addr = pinger.recvfrom(2048)
-            print(message.decode())
-            recvTime = time.time()
-            print(f"The RTT is: {recvTime - startTime}")
-            print("")
-        except TimeoutError:
-            print("Request timed out")
-            print("")
-    
-pinger.close()
+    pinger = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    pinger.settimeout(1)
+
+    for i in range(1, 11): 
+            try:
+                startTime = time.time()
+                pinger.sendto(f"Ping {i} {startTime}".encode(), ('localhost', 12000))
+                # print(f"Ping {i} {startTime}")
+                message, addr = pinger.recvfrom(2048)
+                print(message.decode())
+                recvTime = time.time()
+                print(f"The RTT is: {recvTime - startTime}")
+                print("")
+            except TimeoutError:
+                print("Request timed out")
+                print("")
+        
+    pinger.close()
+
+
+if __name__ == "__main__":
+     ping()
